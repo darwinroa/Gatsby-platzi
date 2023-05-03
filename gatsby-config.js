29adfd6a3aca2fb9b742e8a5715b23c58e7ffa10
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-05-02 20:39:36 -0500

    initial setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Practicing Gatsby`,
+    description: `Este es un proyecto para poder practicar con Gatsby`,
+    author: `@darwinroa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
